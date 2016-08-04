a92195783ee9ec713b0cd69fd9639de1740efd65
Savitha <kssavitha@yahoo.com>
2016-08-04 11:13:03 -0700

    pininterest automation

--- src/pages/HomePage.java
+++ src/pages/HomePage.java
@@ -1,7 +1,9 @@
 package pages;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.xpath.operations.Bool;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,25 +15,87 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 	WebDriver driver;
+
+	public void startup()
+	{
+		System.setProperty("webdriver.chrome.driver", "resource\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.pinterest.com/");
+		driver.manage().window().maximize();
+	}
 	public String goHomePage()
 	{
-		  System.setProperty("webdriver.chrome.driver", "resource\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://www.kayak.com/");
-		driver.manage().window().maximize();
-		WebElement myDynamicElement = (new WebDriverWait(driver, 30))
-				.until(ExpectedConditions.presenceOfElementLocated(By.partialLinkText("FLIGHT")));
-
-		//	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-		String title = driver.getTitle();
-	
-		System.out.println(title);
-	//	WebElement flight = driver.findElement(By.xpath(".//*[@id='cCiv']"));
-		myDynamicElement.click();
-		System.out.println(title);
-		return title;
+		return driver.getTitle();
 	}
+
+	public Boolean logo()
+	{
+		WebElement myDynamicElement = (new WebDriverWait(driver, 30))
+				.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".InspiredWallSignup__logo")));
+
+		return myDynamicElement.isDisplayed();
+	}
+
+	public int links()
+	{
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		List linkcount = driver.findElements(By.xpath("//a[text()='About Pinterest']/../*"));
+		return linkcount.size();
+	}
+
+	public Boolean faceBookbutton()
+	{
+		WebElement fbbutton = (new WebDriverWait(driver, 30))
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-reactid='11']")));
+		return fbbutton.isEnabled();
+	}
+
+
+	public Boolean loginfeilds()
+	{
+		WebElement loginfeilds = (new WebDriverWait(driver, 30))
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form")));
+		return loginfeilds.isDisplayed();
+	}
+
+	public Boolean loginbutton()
+	{
+		WebElement loginbutton = (new WebDriverWait(driver, 30))
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form")));
+		return loginbutton.isEnabled();
+	}
+	public Boolean signupfeilds()
+	{
+		WebElement signupfeilds = (new WebDriverWait(driver, 30))
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form")));
+		return signupfeilds.isDisplayed();
+	}
+
+	public boolean termsandcondtions()
+	{
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebElement myDynamicElement = (new WebDriverWait(driver, 30))
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@data-reactid='32']")));
+
+		return myDynamicElement.isDisplayed();
+	}
+
+	public boolean image()
+	{
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebElement myDynamicElement = (new WebDriverWait(driver, 30))
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-reactid='11']")));
+
+		return myDynamicElement.isDisplayed();
+	}
+
+	public int footer()
+	{
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		List footercount = driver.findElements(By.xpath("//span[text()='Discover Pinterest:']/following-sibling::a"));
+		return footercount.size();
+	}
+
 
 	public void tearDown()
 	{
@@ -39,3 +103,4 @@ public class HomePage {
 	}
 	
 }
+
--- src/test/HomePageTest.java
+++ src/test/HomePageTest.java
@@ -1,21 +1,61 @@
 package test;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import pages.HomePage;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class HomePageTest {
 	
 	pages.HomePage hp = new pages.HomePage();
-	
-@Test
-public void seepageOpened()
+
+	@BeforeTest
+	public void setup()
+	{
+		hp.startup();
+
+	}
+	@AfterTest
+	public void teardown()
+	{
+		hp.tearDown();
+}
+
+	@Test
+public void testpageOpened()
 {
 	String Actual =hp.goHomePage();
-	System.out.println("ACTUAL" + Actual);
-	String Expected = "Search Flights, Hotels & Rental Cars | KAYAK";
-    hp.tearDown();
-	assertEquals(Actual, Expected);
+	String Expected = "Pinterest • The world’s catalog of ideas";
+ 	assertEquals(Actual, Expected);
  }
+	@Test
+	public void testLogo()
+	{
+		Boolean Actual =hp.logo();
+		assertTrue(Actual);
+	}
+	@Test
+	public void testlinks()
+	{
+		int Actual =hp.links();
+		assertEquals(7, Actual);
+	}
+
+	@Test
+	public void testimage()
+	{
+		Boolean Actual =hp.image();
+		assertTrue(Actual);
+	}
+
+
+	@Test
+	public void testfooter()
+	{
+		int Actual =hp.footer();
+		assertEquals(11, Actual);
+	}
 
 }
